5e0efa2a74ada6d73e7cd07e144ce34a59d9aaf5
Deuk_gu_Yoon <deukgu1020@naver.com>
2023-07-04 23:21:43 +0900

    update TIL

--- TIL_VUE_3/01/vue3-project/src/App.vue
+++ TIL_VUE_3/01/vue3-project/src/App.vue
@@ -10,15 +10,42 @@
 >
 <hr />
   <TodoSimpleFoam @add-todo="addTodo"/>
+  <div style="color: red">{{error}}</div>
 
-  <div v-if="!todos.length">
-    <h3>추가된 Todo가 없습니다</h3>
+  <div v-if="!filteredTodos.length">
+    <h3>표시할 데이터가 없습니다</h3>
   </div>
   <TodoList 
     :todos="filteredTodos" 
     @toggle-todo="toggleTodo"
     @delete-todo ="delteTodo"
     />
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li 
+          v-if="currentPage !== 1"
+          class="page-item"
+        >
+          <a style="cursor: pointer" class="page-link" @click="getTodos(currentPage - 1)">
+            Previous
+          </a>
+        </li>
+        <li 
+        v-for="page in numberOfPages"
+        :key="page"
+        class="page-item"
+        :class="currentPage === page ? 'active' : ''"
+        >
+          <a style="cursor: pointer" class="page-link" @click="getTodos(page)">{{page}}</a>
+        </li>
+        <li 
+          v-if="numberOfPages !== currentPage"
+          class="page-item"
+        >
+          <a style="cursor: pointer" class="page-link" @click="getTodos(currentPage + 1)">Next</a>
+        </li>
+      </ul>
+    </nav>
 </div>
 </template>
 
@@ -26,6 +53,7 @@
 import {computed, ref} from 'vue';
 import TodoSimpleFoam from './components/TodoSimpleFoam.vue';
 import TodoList from './components/TodoList.vue';
+import axios from 'axios';
 
 export default {
   components:{
@@ -34,24 +62,78 @@ export default {
   },
   setup() {
 
-    const todos= ref([]); //얘는 app.vue에서 관리해줘야함, TodoList로 옮길수 없음(자식에서 가져와서 부모에 넣고있음)
-    
+    const todos = ref([]); //얘는 app.vue에서 관리해줘야함, TodoList로 옮길수 없음(자식에서 가져와서 부모에 넣고있음)
+    const error = ref('');
+    const numberOfTodos = ref(0);
+    const limit = 5;
+    const currentPage = ref(1);
+
+    const numberOfPages = computed (() => {
+      //computed 사용으로 numberOfTodos(이함수안에서 사용되는 reactive statement임) 가 변경 될 때마다 재연산해줌
+      return Math.ceil(numberOfTodos.value/limit);
+    })
+
+    const getTodos = async (page = currentPage.value) => {
+      currentPage.value = page;
+      try{
+        const res = await axios.get(
+        `http://localhost:3000/todos?_page=${page}&_limit=${limit}`);
+        numberOfTodos.value = res.headers['x-total-count']
+        todos.value = res.data;
+      }catch (err) {
+        console.log(err);
+        error.value = 'something went wrong.';
+      }
+    };
+
+    getTodos()
+
     const todoStyle = {
       textDecoration: 'line-through', color: 'gray'
     };
-
-    const addTodo = (todo)=> {
-      todos.value.push(todo)
+    const addTodo = async (todo)=> {
+      //데이터베이스에 todo 저장
+      error.value = '';
+      
+      try{
+          const res = await axios.post('http://localhost:3000/todos', {
+            subject: todo.subject,
+            completed: todo.completed
+          });
+        todos.value.push(res.data);
+      } catch (err) {
+        console.log(err);
+        error.value = 'something went wrong.';
+      }
     };
 
-    const delteTodo = (index) => {
+    const delteTodo = async (index) => {
+      error.value = '';
+      const id = todos.value[index].id;
+      try{
+      await axios.delete('http://localhost:3000/todos/'+id)
       todos.value.splice(index, 1);
+      } catch(err){
+        console.log(err);
+        error.value = 'something went wrong.';
+      }
+      
     };
 
-    const toggleTodo = (index) => {
+    const toggleTodo = async (index) => {
       //여기서 index는 자식컴포넌트의 toggleTodo()에서 보내준 값이 들어가는 것임
-      todos.value[index].completed = !todos.value[index].completed;
-      console.log(todos.value[index]);
+
+      error.value = '';
+      const id = todos.value[index].id;
+      try{
+        await axios.patch('http://localhost:3000/todos/'+id,{
+            completed: !todos.value[index].completed
+        });
+        todos.value[index].completed = !todos.value[index].completed;
+      }catch(err){
+        console.log(err);
+        error.value = 'something went wrong.';
+      }
     };
 
     const searchText = ref('');
@@ -73,6 +155,10 @@ export default {
       toggleTodo,
       searchText,
       filteredTodos,
+      error,
+      numberOfPages,
+      currentPage,
+      getTodos,
 
     };
 
