14d6dc3849b11f19dd7150faa26b255fc9865ddd
Deuk_gu_Yoon <deukgu1020@naver.com>
2023-07-12 23:32:38 +0900

    update TIL

--- TIL_VUE_3/01/vue3-project/src/App.vue
+++ TIL_VUE_3/01/vue3-project/src/App.vue
@@ -8,7 +8,10 @@
     </li>
   </ul>
 </nav>
-<router-view />
+<div class="container">
+  <router-view />
+</div>
+
 </template>
 
 <script>
--- TIL_VUE_3/01/vue3-project/src/pages/todos/_id.vue
+++ TIL_VUE_3/01/vue3-project/src/pages/todos/_id.vue
@@ -1,14 +1,105 @@
 <template>
-    <div>Todo Page</div>
+    <h1>To-Do Page</h1>
+    <div v-if="loading">
+        Loading...
+    </div>
+    <form 
+        v-else
+        @submit.prevent="onSave"
+    >
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <label>Subject</label>
+                    <input 
+                        v-model="todo.subject" 
+                        type="text" 
+                        class="form-control"
+                    >
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="form-group">
+                    <label>Status</label>
+                    <div>
+                        <button 
+                            type="button"
+                            class="btn"
+                            :class="todo.completed ? 'btn-success':'btn-danger'"
+                            @click="toggleTodoStatus"
+                        >
+                        {{todo.completed ? 'Completed' : 'Incomplete'}}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+            <button 
+                type="submit" 
+                class="btn btn-primary"
+                :disabled="!todoUpdated"
+            >Save</button>
+            <button 
+                class="btn btn-outline-dark ml-2"
+                @click="moveToTodoListPage"
+            >Cancel</button>
+    </form>
 </template>
 
 <script>
-import {useRoute} from 'vue-router'
+import axios from 'axios';
+import {useRoute, useRouter} from 'vue-router'
+import {ref, computed} from 'vue'
+import _ from 'lodash'
 export default {
     setup(){
         const route = useRoute();
+        const router = useRouter();
+        const todo = ref(null);
+        const originalTodo = ref(null);
+        const loading = ref(true);
+        const todoId = route.params.id;
 
-        console.log(route.params.id);
+        const getTodo = async () => {
+            const res = await axios.get(`http://localhost:3000/todos/${todoId}`);
+            todo.value = {...res.data};
+            originalTodo.value = {...res.data};
+
+            loading.value = false;
+        };
+
+        const todoUpdated = computed(() => {
+            return !_.isEqual(todo.value, originalTodo.value)//변경사항이 없을시 save버튼을 disable로 만들기 위함
+        });
+
+        const toggleTodoStatus = () => {
+            todo.value.completed = !todo.value.completed;
+        };
+
+        const moveToTodoListPage = () => {
+            router.push({
+                name: 'Todos'
+            })
+        };
+
+        const onSave = async () => {
+            const res = await axios.put(`http://localhost:3000/todos/${todoId}`, {
+                subject: todo.value.subject,
+                completed: todo.value.completed,
+            });
+            originalTodo.value = {...res.data}//저장후에 save 버튼을 다시 disable로 만들기 위함
+        };
+
+        getTodo();
+        return{
+            todo,
+            loading,
+            toggleTodoStatus,
+            moveToTodoListPage,
+            onSave,
+            todoUpdated
+        }
     }
 }
 </script>
