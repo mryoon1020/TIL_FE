34f3b13590d0540909a65ca1bc13fee8a8308505
Deuk_gu_Yoon <deukgu1020@naver.com>
2023-07-29 23:35:28 +0900

    update TIL

--- TIL_VUE_3/01/vue3-project/src/App.vue
+++ TIL_VUE_3/01/vue3-project/src/App.vue
@@ -11,13 +11,43 @@
 <div class="container">
   <router-view />
 </div>
-
+<Toast
+  v-if="showToast"
+  :message = "toastMessage"
+  :type = "toastAlertType"
+/>
 </template>
 
 <script>
+import Toast from '@/components/Toast.vue';
+import {useToast} from '@/composables/toast';
 
 export default {
-  
+  components: {
+    Toast,
+  },
+
+  setup(){
+      const {
+        showToast,
+        //같은 컴포넌트에서 불러와서 쓰지만 타 컴포넌트에서 사용되는 
+        //동일한 컴포넌트는 다른 스테이트임
+        //따라서 TodoForm.vue에서 사용되는 showToast가 변경 되어도
+        //App.vue에서 사용되는 showToast에는 영향이 가지 않음
+        toastMessage,
+        toastAlertType,
+        triggerToast
+    } = useToast()
+
+    return {
+        showToast,
+        toastMessage,
+        toastAlertType,
+        triggerToast
+    }
+
+  }
+
 }
 </script>
 
